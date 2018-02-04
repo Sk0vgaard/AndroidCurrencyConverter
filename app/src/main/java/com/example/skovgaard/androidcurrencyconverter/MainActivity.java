c6008149ac43346ca75c9b36cf83475cce593538
Sk0vgaard <Sk0vgaard@live.dk>
2018-02-04 20:08:08 +0100

    Now with library instead

--- app/src/main/java/com/example/skovgaard/androidcurrencyconverter/MainActivity.java
+++ app/src/main/java/com/example/skovgaard/androidcurrencyconverter/MainActivity.java
@@ -3,6 +3,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -13,8 +14,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import com.loopj.android.http.*;
 
-import javax.xml.transform.Result;
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,74 +38,109 @@ public class MainActivity extends AppCompatActivity {
         mNOK = findViewById(R.id.nokCurrency);
         mEURO = findViewById(R.id.euroCurrency);
 
-        new GetWebpageTask().execute(currencyURL);
-
+        mConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                async();
+            }
+        });
 
     }
 
-    private String getWebsite(String address) {
+    private void async() {
 
-        StringBuffer stringBuffer = new StringBuffer();
-        BufferedReader reader = null;
-
-        try {
-            URL url = new URL(address);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-
-            reader = new BufferedReader(new InputStreamReader(in));
-            String line = "";
-
-            while ((line = reader.readLine()) != null) {
-                stringBuffer.append(line);
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(currencyURL, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
             }
 
-        } catch (IOException e) {
-            System.out.println("ERROR: " + e);
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    System.out.println("ERROR: " + e);
-                    e.printStackTrace();
-                }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable throwable) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
             }
-        }
-    return stringBuffer.toString();
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+
+        });
     }
 
-    // 1 Params, the type of the parameters sent to the task upon execution.
-    // 2 Progress, the type of the progress units published during the background computation.
-    // 3 Result, the type of the result of the background computation.
-    public class GetWebpageTask extends AsyncTask<String, Void, String> {
+//    private String getWebsite(String address) {
+//
+//        StringBuffer stringBuffer = new StringBuffer();
+//        BufferedReader reader = null;
+//
+//        try {
+//            URL url = new URL(address);
+//            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+//            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//
+//            reader = new BufferedReader(new InputStreamReader(in));
+//            String line = "";
+//
+//            while ((line = reader.readLine()) != null) {
+//                stringBuffer.append(line);
+//            }
+//
+//        } catch (IOException e) {
+//            System.out.println("ERROR: " + e);
+//            e.printStackTrace();
+//        } finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//                } catch (IOException e) {
+//                    System.out.println("ERROR: " + e);
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    return stringBuffer.toString();
+//    }
+//
+//    // 1 Params, the type of the parameters sent to the task upon execution.
+//    // 2 Progress, the type of the progress units published during the background computation.
+//    // 3 Result, the type of the result of the background computation.
+//    public class GetWebpageTask extends AsyncTask<String, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(String... url) {
+//
+//            return getWebsite(url[0]);
+//
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//
+//
+//
+//            super.onPostExecute(result);
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Void... values) {
+//            super.onProgressUpdate(values);
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//
+//            super.onPreExecute();
+//        }
+//    }
+//
 
-        @Override
-        protected String doInBackground(String... url) {
 
-            return getWebsite(url[0]);
 
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            Log.i("Catching", result);
-
-            super.onPostExecute(result);
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPreExecute() {
-
-            super.onPreExecute();
-        }
-    }
 
 }
